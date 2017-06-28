9dbef17349bf305ce4183f8165f729ed89b73b2f
jeoeveren <jeroenvanoeveren@hotmail.com>
2017-06-28 14:38:40 +0200

    Define the Ingredients

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -1,9 +1,24 @@
+#method
 def print_divider
   puts "*" * 40
   puts "\n"
 end
 
+#method
 def print_progress_bar
   3.times { sleep 0.5; print "." }
   puts "\n"
 end
+
+#some constants
+NUM_POTATOES = 5
+NUM_EGGS = 6
+
+#hash index
+ingredients = [
+  { name: 'potatoes', quantity: NUM_POTATOES },
+  { name: 'eggs', quantity: NUM_EGGS },
+  { name: 'onion', quantity: 1 },
+  { name: 'oil', quantity: 'some' },
+  { name: 'salt', quantity: 'some' },
+]
